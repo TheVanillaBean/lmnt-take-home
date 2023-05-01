342c5dc0e19cc7e9692be54f9e41a28e648361f2
Alex Alimov <aalimov@asu.edu>
2023-04-30 20:48:29 -0700

    Add Head tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import CartProvider from 'components/context/CartContext';
 import 'components/styles/globals.css';
 import { Nanum_Gothic, Open_Sans } from 'next/font/google';
+import Head from 'next/head';
 
 // If loading a variable font, you don't need to specify the font weight
 const NanumGothic = Nanum_Gothic({
@@ -20,6 +21,11 @@ const OpenSans = Open_Sans({
 export default function App({ Component, pageProps }) {
   return (
     <CartProvider>
+      <Head>
+        <title>LMNT Bundle Builder</title>
+        <meta name='description' content='Save 25% by building your own bundle' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      </Head>
       <Component className={`${NanumGothic.variable} ${OpenSans.variable}`} {...pageProps} />{' '}
     </CartProvider>
   );
